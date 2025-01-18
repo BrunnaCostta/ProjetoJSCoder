78fab55bda44285f88dfd1a4e49f9987c343a908
Bruna Costa <brunadacosta2107@gmail.com>
2025-01-18 18:36:31 -0300

    tela inicial em andamento

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  safelist: [
+    "text-orange-500",
+    "text-orange-400",
+    "text-green-500",
+    "text-base",
+    "text-5xl",
+    "text-4xl",
+    "text-green-500",
+    "bg-slate-800",
+    "bg-slate-700",
+  ],
   content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
     extend: {},
